c4685817b5870167fa36e7f36dd7115b9e1d02ba
abubakarelsadeeq521 <abubakarelsadeeq521@gmail.com>
2024-09-16 12:47:34 +0100

    add composer to dashboard

---
+++ dashboard/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sadeeq/dashboard',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1fda81e608926de2bc5bb87e0306f6fa8ba4d9a7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.1',
+            'version' => '6.10.1.0',
+            'reference' => '500501c2ce893c824c801da135d02661199f60c5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sadeeq/dashboard' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1fda81e608926de2bc5bb87e0306f6fa8ba4d9a7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
