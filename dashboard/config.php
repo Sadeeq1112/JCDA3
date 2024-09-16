8be1bee0d0c5def78c8e3fd2da8b433568e846ff
abubakarelsadeeq521 <abubakarelsadeeq521@gmail.com>
2024-09-16 12:19:49 +0100

    add private keys/ to be removed later

--- dashboard/config.php
+++ dashboard/config.php
@@ -1,7 +1,7 @@
 <?php
 // File: config.php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'jcda_db');
-define('DB_USER', 'your_username');
-define('DB_PASS', 'your_password');
+define('DB_NAME', 'bfsllkdc_membership');
+define('DB_USER', 'bfsllkdc_member');
+define('DB_PASS', 'd6C(YdA62yWm9-');
 define('JWT_SECRET', 'yH!8xP#mK$qW2zL&nF5vC7bE9jT3sA6d');
