58f9226a7986f0e23b5a32310c3ff68df1362903
abubakarelsadeeq521 <abubakarelsadeeq521@gmail.com>
2024-09-16 21:36:32 +0100

    update to paymet page

--- dashboard/public/payment.php
+++ dashboard/public/payment.php
@@ -3,9 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'includes/config.php';
-require_once 'includes/db.php';
-require_once 'includes/functions.php';
+require_once '../includes/config.php';
+require_once '../includes/db.php';
+require_once '../includes/functions.php';
 
 // Start session if not already started
 if (session_status() == PHP_SESSION_NONE) {
