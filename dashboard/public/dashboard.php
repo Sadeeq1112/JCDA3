3200aa620129d15b43fbc51dea584da6e29b0f29
Sadeeq1112 <2019ns0974@unijos.edu.ng>
2024-09-22 17:01:43 +0100

    changes to dashboard

--- dashboard/public/dashboard.php
+++ dashboard/public/dashboard.php
@@ -57,7 +57,7 @@ $latest_payment = $stmt->fetch(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JCDA - Dashboard</title>
-    <link rel="icon" href="public/JCDA White.png" type="image/png">
+    <link rel="icon" href="public/JCDA White.png" type="../JCDA WHite.png">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
     <style>
@@ -235,7 +235,7 @@ $latest_payment = $stmt->fetch(PDO::FETCH_ASSOC);
     <div class="dashboard">
         <div class="sidebar" id="sidebar">
             <div class="logo">
-                <img src="public/JCDA White.png" alt="JCDA Logo">
+                <img src="../JCDA White.png" alt="JCDA Logo">
             </div>
             <ul>
                 <li><a href="#" class="active"><i class="fas fa-home sidebar-icon"></i> <span class="sidebar-text">Home</span></a></li>
